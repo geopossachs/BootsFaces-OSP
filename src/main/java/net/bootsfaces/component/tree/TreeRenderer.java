882757e802d2f3b076f655a5119ff08eb8326b2c
geopossachs <10324450+geopossachs@users.noreply.github.com>
2019-09-05 00:20:32 +0200

    change the ajax behavior by the tree
    Signed-off-by: geopossachs <10324450+geopossachs@users.noreply.github.com>

--- src/main/java/net/bootsfaces/component/tree/TreeRenderer.java
+++ src/main/java/net/bootsfaces/component/tree/TreeRenderer.java
@@ -44,157 +44,170 @@ import net.bootsfaces.utils.BsfUtils;
 @FacesRenderer(componentFamily = C.BSFCOMPONENT, rendererType = "net.bootsfaces.component.tree.Tree")
 public class TreeRenderer extends CoreRenderer {
 
-    /**
-     * Decode ajax behaviours specific to the components
-     *
-     * @param context
-     * @param component
-     */
-    @Override
-    public void decode(FacesContext context, UIComponent component) {
-        Tree tree = (Tree) component;
-        super.decode(context, tree);
+	/**
+	 * Decode ajax behaviours specific to the components
+	 *
+	 * @param context
+	 * @param component
+	 */
+	@Override
+	public void decode(FacesContext context, UIComponent component) {
+		Tree tree = (Tree) component;
+		super.decode(context, tree);
 
-        final TreeNodeEventListener nodeSelectionListener = tree.getNodeSelectionListener();
-        String params = context.getExternalContext().getRequestParameterMap().get("BsFEvent");
+		final TreeNodeEventListener nodeSelectionListener = tree.getNodeSelectionListener();
+		String params = context.getExternalContext().getRequestParameterMap().get("BsFEvent");
 
-        if (params != null) {
-            String[] pair = params.split(":", 2);
-            String key = pair[0];
-            Integer value = null;
-            if (pair.length == 2)
-                value = Integer.valueOf(pair[1]);
-            
-            if (value != null) {
-                Node n = TreeModelUtils.searchNodeById(tree.getValue(), value);
+		if (params != null) {
+			String[] pair = params.split(":", 2);
+			String key = pair[0];
+			Integer value = null;
+			if (pair.length == 2) {
+				value = Integer.valueOf(pair[1]);
+			}
 
-                if (nodeSelectionListener != null && n != null) {
-                    // execute listener only for listened events
-                    if ("nodeSelected".equals(key)) {
-                        n.setSelected(true);
-                        nodeSelectionListener.processValueSelected(new TreeNodeSelectionEvent(n, true));
-                    } else if ("nodeUnselected".equals(key)) {
-                        n.setSelected(false);
-                        nodeSelectionListener.processValueSelected(new TreeNodeSelectionEvent(n, false));
-                    } else if ("nodeChecked".equals(key)) {
-                        n.setChecked(true);
-                        nodeSelectionListener.processValueChecked(new TreeNodeCheckedEvent(n, true));
-                    } else if ("nodeUnchecked".equals(key)) {
-                        n.setChecked(false);
-                        nodeSelectionListener.processValueChecked(new TreeNodeCheckedEvent(n, false));
-                    } else if ("nodeExpanded".equals(key)) {
-                        n.setExpanded(true);
-                        nodeSelectionListener.processValueExpanded(new TreeNodeExpandedEvent(n, true));
-                    } else if ("nodeCollapsed".equals(key)) {
-                        n.setExpanded(false);
-                        nodeSelectionListener.processValueExpanded(new TreeNodeExpandedEvent(n, false));
-                    }
-                }
-            }
-        }
-    }
+			if (value != null) {
+				Node n = TreeModelUtils.searchNodeById(tree.getValue(), value);
 
-    @Override
-    public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
-        if (!component.isRendered()) {
-            return;
-        }
+				if (nodeSelectionListener != null && n != null) {
+					// execute listener only for listened events
+					if ("nodeSelected".equals(key)) {
+						n.setSelected(true);
+						nodeSelectionListener.processValueSelected(new TreeNodeSelectionEvent(n, true));
+					} else if ("nodeUnselected".equals(key)) {
+						n.setSelected(false);
+						nodeSelectionListener.processValueSelected(new TreeNodeSelectionEvent(n, false));
+					} else if ("nodeChecked".equals(key)) {
+						n.setChecked(true);
+						nodeSelectionListener.processValueChecked(new TreeNodeCheckedEvent(n, true));
+					} else if ("nodeUnchecked".equals(key)) {
+						n.setChecked(false);
+						nodeSelectionListener.processValueChecked(new TreeNodeCheckedEvent(n, false));
+					} else if ("nodeExpanded".equals(key)) {
+						n.setExpanded(true);
+						nodeSelectionListener.processValueExpanded(new TreeNodeExpandedEvent(n, true));
+					} else if ("nodeCollapsed".equals(key)) {
+						n.setExpanded(false);
+						nodeSelectionListener.processValueExpanded(new TreeNodeExpandedEvent(n, false));
+					}
+				}
+			}
+		}
+	}
 
-        Tree tree = (Tree) component;
-        ResponseWriter rw = context.getResponseWriter();
-        String clientId = tree.getClientId();
+	@Override
+	public void encodeBegin(FacesContext context, UIComponent component) throws IOException {
+		if (!component.isRendered()) {
+			return;
+		}
 
-        // check is inside form (MyFaces requires to be)
-        final UIForm form = BsfUtils.getClosestForm(tree);
-        if (form == null) {
-            throw new FacesException("The tree component must be inside a form", null);
-        }
+		Tree tree = (Tree) component;
+		ResponseWriter rw = context.getResponseWriter();
+		String clientId = tree.getClientId();
 
-        rw.startElement("div", tree);
-        rw.writeAttribute("id", "tree_" + clientId, "id");
-        renderPassThruAttributes(context, component, new String[] { "placeholder", "tabindex", "lang", "accesskey"}, true);
-        String clazz = Responsive.getResponsiveStyleClass(tree, false);
-        String styleClass = tree.getStyleClass();
-        if (null != styleClass) {
-            clazz = styleClass + clazz;
-        }
-        clazz = clazz.trim();
-        if (clazz.length() > 0) {
-            rw.writeAttribute("class", clazz, "class");
-        }
-        writeAttribute(rw, "style", tree.getStyle());
-        rw.endElement("div");
-    }
+		// check is inside form (MyFaces requires to be)
+		final UIForm form = BsfUtils.getClosestForm(tree);
+		if (form == null) {
+			throw new FacesException("The tree component must be inside a form", null);
+		}
 
-    @Override
-    public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
-        if (!component.isRendered()) {
-            return;
-        }
-        Tree tree = (Tree) component;
+		rw.startElement("div", tree);
+		rw.writeAttribute("id", "tree_" + clientId, "id");
+		renderPassThruAttributes(context, component, new String[]{"placeholder", "tabindex", "lang", "accesskey"}, true);
+		String clazz = Responsive.getResponsiveStyleClass(tree, false);
+		String styleClass = tree.getStyleClass();
+		if (null != styleClass) {
+			clazz = styleClass + clazz;
+		}
+		clazz = clazz.trim();
+		if (clazz.length() > 0) {
+			rw.writeAttribute("class", clazz, "class");
+		}
+		writeAttribute(rw, "style", tree.getStyle());
+		rw.endElement("div");
+	}
 
-        String clientId = tree.getClientId();
-        String jqClientId = BsfUtils.escapeJQuerySpecialCharsInSelector(clientId);
-        ResponseWriter rw = context.getResponseWriter();
+	@Override
+	public void encodeEnd(FacesContext context, UIComponent component) throws IOException {
+		if (!component.isRendered()) {
+			return;
+		}
+		Tree tree = (Tree) component;
 
-        final UIForm form = BsfUtils.getClosestForm(tree);
-        if (form == null) {
-            throw new FacesException("The tree component must be inside a form", null);
-        }
-        // String updateItems = BsfUtils.GetOrDefault("'" + tree.getUpdate() + "'", "null");
-        String updateItems = tree.getUpdate();
-        if (updateItems != null) {
-            updateItems = ExpressionResolver.getComponentIDs(context, component, updateItems);
-        }
+		String clientId = tree.getClientId();
+		String jqClientId = BsfUtils.escapeJQuerySpecialCharsInSelector(clientId);
+		ResponseWriter rw = context.getResponseWriter();
 
-        rw.startElement("script", tree);
-        //# Start enclosure
-        rw.writeText("$(document).ready(function() {", null);
-        // build tree management javascript
-        rw.writeText("function getTreeData() { "
-                + "   return '" + TreeModelUtils.renderModelAsJson(tree.getValue(), tree.isRenderRoot()) + "'; "
-                + "} "
-                + // build tree structure
-                "$('#tree_" + jqClientId + "').treeview({ "
-                + (tree.isShowTags() ? "showTags: true," : "")
-                + (tree.isShowIcon() ? "showIcon: true," : "")
-                + (tree.isShowCheckbox() ? "showCheckbox: true," : "")
-                + (tree.isEnableLinks() ? "enableLinks: true," : "")
-                + (tree.isShowBorder() ? "showBorder: true," : "showBorder: false,")
-                + (BsfUtils.isStringValued(tree.getCollapseIcon()) ? "collapseIcon: '" + tree.getCollapseIcon() + "'," : "")
-                + (BsfUtils.isStringValued(tree.getExpandIcon()) ? "expandIcon: '" + tree.getExpandIcon() + "'," : "")
-                + (BsfUtils.isStringValued(tree.getColor()) ? "color: '" + tree.getColor() + "'," : "")
-                + (BsfUtils.isStringValued(tree.getHoverColor()) ? "onhoverColor: '" + tree.getHoverColor() + "'," : "")
-                + (BsfUtils.isStringValued(tree.getSelectedColor()) ? "selectedColor: '" + tree.getSelectedColor() + "'," : "")
-                + (BsfUtils.isStringValued(tree.getBorderColor()) ? "borderColor: '" + tree.getBorderColor() + "'," : "")
-                + "   data: getTreeData(),   "
-                + // enable nodeSelected event callback
-                "	onNodeSelected: function(event, data) { "
-                + "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeSelected:' + data.nodeInternalId);"
-                + "	},"
-                + // enable nodeUnselected event callback
-                "	onNodeUnselected: function(event, data) { "
-                + "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeUnselected:' + data.nodeInternalId);"
-                + "	},"
-                + //enable nodeChecked event callback
-                "	onNodeChecked: function(event, data) { "
-                + "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeChecked:' + data.nodeInternalId);"
-                + "	},"
-                + //enable nodeUnchecked event callback
-                "	onNodeUnchecked: function(event, data) { "
-                + "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeUnchecked:' + data.nodeInternalId);"
-                + "	},"
-                + // enable nodeCollapsed event callback
-                "	onNodeCollapsed: function(event, data) { "
-                + "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeCollapsed:' + data.nodeInternalId);"
-                + "	},"
-                + // enable nodeExpanded event callback
-                "	onNodeExpanded: function(event, data) { "
-                + "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeExpanded:' + data.nodeInternalId);"
-                + // @all
-                "	}"
-                + "}); ", null);
-        rw.writeText("});", null);
-        rw.endElement("script");
-    }
+		// per default ajax behavior is disabled
+		boolean activateAjaxBehavior = false;
+
+		//FIXME - can we remove this next line of code?
+		// String updateItems = BsfUtils.GetOrDefault("'" + tree.getUpdate() + "'", "null");
+		String updateItems = tree.getUpdate();
+		if (updateItems != null) {
+			activateAjaxBehavior = true;
+			updateItems = ExpressionResolver.getComponentIDs(context, component, updateItems);
+		}
+
+		// make the secound check for ajax behavior
+		if (tree.getNodeSelectionListener() != null && !activateAjaxBehavior) {
+			activateAjaxBehavior = true;
+		}
+
+		final UIForm form = BsfUtils.getClosestForm(tree);
+		if (activateAjaxBehavior && form == null) {
+			// we need a <form> for ajax call's
+			throw new FacesException("The tree component must be inside a form", null);
+		}
+
+		rw.startElement("script", tree);
+		//# Start enclosure
+		rw.writeText("$(document).ready(function() {", null);
+		// build tree management javascript
+		rw.writeText("function getTreeData() { "
+			+ "   return '" + TreeModelUtils.renderModelAsJson(tree.getValue(), tree.isRenderRoot()) + "'; "
+			+ "} "
+			+ // build tree structure
+			"$('#tree_" + jqClientId + "').treeview({ "
+			+ (tree.isShowTags() ? "showTags: true," : "")
+			+ (tree.isShowIcon() ? "showIcon: true," : "")
+			+ (tree.isShowCheckbox() ? "showCheckbox: true," : "")
+			+ (tree.isEnableLinks() ? "enableLinks: true," : "")
+			+ (tree.isShowBorder() ? "showBorder: true," : "showBorder: false,")
+			+ (BsfUtils.isStringValued(tree.getCollapseIcon()) ? "collapseIcon: '" + tree.getCollapseIcon() + "'," : "")
+			+ (BsfUtils.isStringValued(tree.getExpandIcon()) ? "expandIcon: '" + tree.getExpandIcon() + "'," : "")
+			+ (BsfUtils.isStringValued(tree.getColor()) ? "color: '" + tree.getColor() + "'," : "")
+			+ (BsfUtils.isStringValued(tree.getHoverColor()) ? "onhoverColor: '" + tree.getHoverColor() + "'," : "")
+			+ (BsfUtils.isStringValued(tree.getSelectedColor()) ? "selectedColor: '" + tree.getSelectedColor() + "'," : "")
+			+ (BsfUtils.isStringValued(tree.getBorderColor()) ? "borderColor: '" + tree.getBorderColor() + "'," : "")
+			+ "   data: getTreeData(),   "
+			+ (activateAjaxBehavior ? ( // enable nodeSelected event callback
+				"	onNodeSelected: function(event, data) { "
+				+ "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeSelected:' + data.nodeInternalId);"
+				+ "	},"
+				+ // enable nodeUnselected event callback
+				"	onNodeUnselected: function(event, data) { "
+				+ "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeUnselected:' + data.nodeInternalId);"
+				+ "	},"
+				+ //enable nodeChecked event callback
+				"	onNodeChecked: function(event, data) { "
+				+ "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeChecked:' + data.nodeInternalId);"
+				+ "	},"
+				+ //enable nodeUnchecked event callback
+				"	onNodeUnchecked: function(event, data) { "
+				+ "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeUnchecked:' + data.nodeInternalId);"
+				+ "	},"
+				+ // enable nodeCollapsed event callback
+				"	onNodeCollapsed: function(event, data) { "
+				+ "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeCollapsed:' + data.nodeInternalId);"
+				+ "	},"
+				+ // enable nodeExpanded event callback
+				"	onNodeExpanded: function(event, data) { "
+				+ "   	BsF.ajax.callAjax(this, event, '" + updateItems + "', '" + clientId + "', null, null, null, 'nodeExpanded:' + data.nodeInternalId);"
+				+ // @all
+				"	}") : "")
+			+ "}); ", null);
+		rw.writeText("});", null);
+		rw.endElement("script");
+	}
 }

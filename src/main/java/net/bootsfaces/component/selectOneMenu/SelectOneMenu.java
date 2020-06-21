7bfaaa39a40051be1f533ba6f42ecb4e697c332c
Georg Posselt <georg.posselt@3m5.de>
2020-06-21 23:06:24 +0200

    add the validator-message to the taglib

--- src/main/java/net/bootsfaces/component/selectOneMenu/SelectOneMenu.java
+++ src/main/java/net/bootsfaces/component/selectOneMenu/SelectOneMenu.java
@@ -3,22 +3,20 @@
  *
  *  This file is part of BootsFaces.
  *
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package net.bootsfaces.component.selectOneMenu;
 
-import javax.faces.context.FacesContext;
 import javax.faces.event.AbortProcessingException;
 import javax.faces.event.ComponentSystemEvent;
 import javax.faces.event.ListenerFor;
@@ -43,91 +41,103 @@ import net.bootsfaces.render.IResponsiveLabel;
 import net.bootsfaces.render.Tooltip;
 import net.bootsfaces.utils.BsfUtils;
 
-/** This class holds the attributes of &lt;b:selectOneMenu /&gt;. */
-@ListenersFor({ @ListenerFor(systemEventClass = PostAddToViewEvent.class) })
+/**
+ * This class holds the attributes of &lt;b:selectOneMenu /&gt;.
+ */
+@ListenersFor({
+    @ListenerFor(systemEventClass = PostAddToViewEvent.class)})
 @FacesComponent(SelectOneMenu.COMPONENT_TYPE)
 public class SelectOneMenu extends SelectOneMenuCore implements net.bootsfaces.render.IHasTooltip, IAJAXComponent, IAJAXComponent2, IResponsive, IResponsiveLabel {
 
-	public static final String COMPONENT_TYPE = C.BSFCOMPONENT + ".selectOneMenu.SelectOneMenu";
+    public static final String COMPONENT_TYPE = C.BSFCOMPONENT + ".selectOneMenu.SelectOneMenu";
 
-	public static final String COMPONENT_FAMILY = C.BSFCOMPONENT;
+    public static final String COMPONENT_FAMILY = C.BSFCOMPONENT;
 
-	public static final String DEFAULT_RENDERER = "net.bootsfaces.component.selectOneMenu.SelectOneMenu";
+    public static final String DEFAULT_RENDERER = "net.bootsfaces.component.selectOneMenu.SelectOneMenu";
 
-	public SelectOneMenu() {
-		Tooltip.addResourceFiles();
-		//!bs-less//AddResourcesListener.addThemedCSSResource("dropdowns.css");
-		AddResourcesListener.addThemedCSSResource("core.css");
-		AddResourcesListener.addExtCSSResource("select2.min.css");
-		//AddResourcesListener.addThemedCSSResource("bsf.css");
-		AddResourcesListener.addBasicJSResource(C.BSF_LIBRARY, "js/select2.min.js");
-		setRendererType(DEFAULT_RENDERER);
-	}
+    public SelectOneMenu() {
+        Tooltip.addResourceFiles();
+        //!bs-less//AddResourcesListener.addThemedCSSResource("dropdowns.css");
+        AddResourcesListener.addThemedCSSResource("core.css");
+        AddResourcesListener.addExtCSSResource("select2.min.css");
+        //AddResourcesListener.addThemedCSSResource("bsf.css");
+        AddResourcesListener.addBasicJSResource(C.BSF_LIBRARY, "js/select2.min.js");
+        setRendererType(DEFAULT_RENDERER);
+    }
 
-	public void setValueExpression(String name, ValueExpression binding) {
-		name = BsfUtils.snakeCaseToCamelCase(name);
-		super.setValueExpression(name, binding);
-	}
+    @Override
+    public void setValueExpression(String name, ValueExpression binding) {
+        name = BsfUtils.snakeCaseToCamelCase(name);
+        super.setValueExpression(name, binding);
+    }
 
-	private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(
-			Arrays.asList("blur", "change", "click", "dblclick", "focus", "keydown", "keypress", "keyup",
-					"mousedown", "mousemove", "mouseout", "mouseover", "mouseup", "select"));
+    private static final Collection<String> EVENT_NAMES = Collections.unmodifiableCollection(
+            Arrays.asList("blur", "change", "click", "dblclick", "focus", "keydown", "keypress", "keyup",
+                    "mousedown", "mousemove", "mouseout", "mouseover", "mouseup", "select"));
 
-	/**
-	 * returns the subset of AJAX requests that are implemented by jQuery
-	 * callback or other non-standard means (such as the onclick event of
-	 * b:tabView, which has to be implemented manually).
-	 *
-	 * @return
-	 */
-	public Map<String, String> getJQueryEvents() {
-		return null;
-	}
+    /**
+     * returns the subset of AJAX requests that are implemented by jQuery
+     * callback or other non-standard means (such as the onclick event of
+     * b:tabView, which has to be implemented manually).
+     *
+     * @return
+     */
+    @Override
+    public Map<String, String> getJQueryEvents() {
+        return null;
+    }
 
-	/**
-	 * Returns the parameter list of jQuery and other non-standard JS callbacks.
-	 * If there's no parameter list for a certain event, the default is simply "event".
-	 * 
-	 * @return A hash map containing the events. May be null.
-	 */
-	@Override
-	public Map<String, String> getJQueryEventParameterLists() {
-		return null;
-	}
+    /**
+     * Returns the parameter list of jQuery and other non-standard JS callbacks.
+     * If there's no parameter list for a certain event, the default is simply
+     * "event".
+     *
+     * @return A hash map containing the events. May be null.
+     */
+    @Override
+    public Map<String, String> getJQueryEventParameterLists() {
+        return null;
+    }
 
-	/**
-	 * Returns the subset of the parameter list of jQuery and other non-standard JS callbacks which is sent to the server via AJAX.
-	 * If there's no parameter list for a certain event, the default is simply null.
-	 * 
-	 * @return A hash map containing the events. May be null.
-	 */
-	@Override
-	public Map<String, String> getJQueryEventParameterListsForAjax() {
-		return null;
-	}
+    /**
+     * Returns the subset of the parameter list of jQuery and other non-standard
+     * JS callbacks which is sent to the server via AJAX. If there's no
+     * parameter list for a certain event, the default is simply null.
+     *
+     * @return A hash map containing the events. May be null.
+     */
+    @Override
+    public Map<String, String> getJQueryEventParameterListsForAjax() {
+        return null;
+    }
 
-	public Collection<String> getEventNames() {
-		return EVENT_NAMES;
-	}
+    @Override
+    public Collection<String> getEventNames() {
+        return EVENT_NAMES;
+    }
 
-	public String getDefaultEventName() {
-		return "change";
-	}
+    @Override
+    public String getDefaultEventName() {
+        return "change";
+    }
 
-	public void validateValue(FacesContext context, Object newValue) {
-		super.validateValue(context, newValue);
-	}
+    @Override
+    public void validateValue(FacesContext context, Object newValue) {
+        super.validateValue(context, newValue);
+    }
 
-		public void processEvent(ComponentSystemEvent event) throws AbortProcessingException {
-		if (isAutoUpdate()) {
-			if (FacesContext.getCurrentInstance().isPostback()) {
-				FacesContext.getCurrentInstance().getPartialViewContext().getRenderIds().add(getClientId());
-			}
- 	 		super.processEvent(event);
- 	 	}
-	}
+    @Override
+    public void processEvent(ComponentSystemEvent event) throws AbortProcessingException {
+        if (isAutoUpdate()) {
+            if (FacesContext.getCurrentInstance().isPostback()) {
+                FacesContext.getCurrentInstance().getPartialViewContext().getRenderIds().add(getClientId());
+            }
+            super.processEvent(event);
+        }
+    }
 
-public String getFamily() {
-		return COMPONENT_FAMILY;
-	}
+    @Override
+    public String getFamily() {
+        return COMPONENT_FAMILY;
+    }
 }
